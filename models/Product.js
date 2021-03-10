727d6d3f7ce7250ebee508b8ddcb8ea5c0e4b929
Tony Tran <codemonkey713@gmail.com>
2021-03-09 22:34:24 -0600

    corrected the error message for validation on product model

--- models/Product.js
+++ models/Product.js
@@ -23,13 +23,17 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
-      validate: DECIMAL,
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      validate: NUMBER,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
